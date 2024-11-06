4a5a5acd53b4ee2cdc29dfd20f98153725ec223e
Excavator <kvronin@gmail.com>
2024-11-07 03:46:04 +0500

    add fonts

--- app/index.jsx
+++ app/index.jsx
@@ -5,7 +5,7 @@ import { Link } from "expo-router";
 export default function App() {
   return (
     <View className="flex-1 items-center justify-center bg-white">
-      <Text className="text-3xl">Hello My Lord!!</Text>
+      <Text className="text-3xl font-pblack">Hello My Lord!!</Text>
       <StatusBar style="auto" />
       <Link href="/profile" style={{ color: "blue" }}>
         Go to Profile
